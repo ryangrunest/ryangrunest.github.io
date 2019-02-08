569045467f85d5fdd406c219ece1a1f9b212140b
Ryan Grunest <ryangrunest45@gmail.com>
2019-02-08 14:56:25 -0800

    update portfolio page

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+// click listener for stickynotes portfolio
+$("#stickynote-card").on("click", function() {
+	window.open("https://ryangrunest.github.io/sticky-notes/");
+});
+// click listener for word guess portfolio
+$("#word-guess-card").on("click", function() {
+	window.open("https://ryangrunest.github.io/Word-Guess-Game/");
+});
+// click listener for tic tac toe portfolio
+$("#tic-tac-toe-card").on("click", function() {
+	window.open("https://ryangrunest.github.io/Tic-Tac-Toe/");
+});
